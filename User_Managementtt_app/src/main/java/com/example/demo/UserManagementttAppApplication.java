ac8db3b48688c581480910cd6d2639c6ad790fe2
aditi.singh <aditi.singh@kellton.com>
2024-08-22 20:35:55 +0530

    my project

---
+++ User_Managementtt_app/src/main/java/com/example/demo/UserManagementttAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementttAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementttAppApplication.class, args);
+	}
+
+}
